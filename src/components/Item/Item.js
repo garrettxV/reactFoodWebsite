d1da3f6d3f5b698b1cf65f15695854c2d3185ce3
garrettxV <77716681+garrettxV@users.noreply.github.com>
2021-02-14 23:21:43 -0600

    made changes to Item.js and FoodMenu.js

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,13 +1,8 @@
 const Item = ({ item }) => {
-	// const imageStyle = {
-	// 	background: `url(${item.image}) no-repeat center center fixed`
-	// };
-
 	return (
 		<div>
 			<div className="item" />
 			<div className="item__header">
-				{/* <p>{item.food}</p> */}
 				<img src={item.image} alt="food" />
 			</div>
 			<div className="item__footer">
