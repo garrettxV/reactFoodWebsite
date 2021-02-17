92179675640329e5e4ef17eb0ad568bd8526a58c
garrettxV <77716681+garrettxV@users.noreply.github.com>
2021-02-17 17:59:12 -0600

    updated SignUpLogin components, fixed positioning issues.

--- src/components/SignUpLogin/SignUpLogin.js
+++ src/components/SignUpLogin/SignUpLogin.js
@@ -1,26 +1,37 @@
 import './SignUpLogin.css';
-import { IoIosAdd } from "react-icons/io";
+import { IoIosAdd } from 'react-icons/io';
 
-const SignUpLogin = props => {
+const SignUpLogin = (props) => {
 	return (
 		<div className="contact__card-backdrop">
 			<div className="contact__card-container">
-			<div className="contact__card">
-				<button className="contact__card-close" onClick={props.contactHandler}><IoIosAdd /></button>
-				<h2 className="contact-card_greeting"> Sign In</h2>
-				<div className="form__container">
-					<form className="contact-card_form" action="">
-						<input type="email" name="name" id="Name" placeholder="Email" className="contact-card_input" />
-						<input
-							type="password"
-							name="email"
-							id="email"
-							placeholder="Password"
-							className="contact-card_input"
-						/>
-					</form>
+				<div className="contact__card">
+					<div className="contact__card-close-container">
+						<div className="contact__spacer" />
+						<button className="contact__card-close" onClick={props.contactHandler}>
+							<IoIosAdd />
+						</button>
+					</div>
+					<h2 className="contact-card_greeting"> Sign In</h2>
+					<div className="form__container">
+						<form className="contact-card_form" action="">
+							<input
+								type="email"
+								name="name"
+								id="Name"
+								placeholder="Email"
+								className="contact-card_input"
+							/>
+							<input
+								type="password"
+								name="email"
+								id="email"
+								placeholder="Password"
+								className="contact-card_input"
+							/>
+						</form>
+					</div>
 				</div>
-			</div>
 			</div>
 		</div>
 	);
